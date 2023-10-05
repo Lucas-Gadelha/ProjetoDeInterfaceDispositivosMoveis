025c9ddbe530c065a14e24228289cc1b2100e947
Lucas-Gadelha <91857879+Lucas-Gadelha@users.noreply.github.com>
2023-10-05 12:40:49 -0300

    Add files via upload
    tentando  upar

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import PokemonApp from './components/16Pokemon/PokemonApp.jsx'
+// import PokemonApp from './components/15Navigation02/05Pokemon/PokemonApp.jsx';
+// import MainScreen from './components/15NavigationStack/MainScreen.jsx'
+
+export default function App() {
+  return (
+
+      <PokemonApp />
+
+  );
+}
+
