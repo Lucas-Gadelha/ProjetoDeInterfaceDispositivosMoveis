131252bcc18d558ebb792703412ab82eb096db9a
Lucas-Gadelha <91857879+Lucas-Gadelha@users.noreply.github.com>
2023-10-05 13:26:04 -0300

    Add files via upload
    atualizando

--- introducao/App.js
+++ introducao/App.js
@@ -1,33 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Questao01 from './components/tarefa01/Questao01';
-import Questao03 from './components/tarefa01/Questao03';
-import MainScreen from './components/15NavigationStack/MainScreen';
-// import PokemonApp from './components/16Pokemon/PokemonApp';
-import MainNavApp from './components/15Navigation02/01NavStack/MainNavApp';
-import PokemonApp from './components/15Navigation02/05Pokemon/PokemonApp';
-
+import PokemonApp from './components/16Pokemon/PokemonApp.jsx'
+// import PokemonApp from './components/15Navigation02/05Pokemon/PokemonApp.jsx';
+// import MainScreen from './components/15NavigationStack/MainScreen.jsx'
 
 export default function App() {
   return (
-    <View style={styles.container}>
 
-      {/* <Questao01 />
-      <Questao03 cor='blue'/> */}
       <PokemonApp />
-    </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: '#c0c0c0',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 50
-  },
-});
